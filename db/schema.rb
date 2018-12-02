7dbf91f7e3cc09f5777bbb964ca3af2f566d5785
mostafa-elmenbawy <elmenabawym@gmail.com>
2018-12-02 23:18:48 +0100

    Change duration attribute in movies from integer to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_211750) do
+ActiveRecord::Schema.define(version: 2018_12_02_220940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,11 +25,11 @@ ActiveRecord::Schema.define(version: 2018_12_02_211750) do
 
   create_table "movies", force: :cascade do |t|
     t.string "name"
-    t.integer "duration"
     t.date "release_date"
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "duration"
   end
 
   create_table "tickets", force: :cascade do |t|
